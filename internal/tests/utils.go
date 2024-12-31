c93b4310794da385fccf678968331b1d12bc5816
Piotr Kowalski <piotr.98@icloud.com>
2024-12-31 10:19:55 +0100

    Update existing tests to handle user ids

--- internal/tests/utils.go
+++ internal/tests/utils.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"github.com/KowalskiPiotr98/gotabase"
 	"github.com/google/uuid"
 	"math/rand"
 	"time"
@@ -13,4 +14,16 @@ func GetRandomUuid() uuid.UUID {
 
 func GetRandomTestTime() time.Time {
 	return time.Date(2000, time.Month((rand.Int()%12)+1), (rand.Int()%20)+1, 0, 0, 0, 0, time.UTC)
+}
+
+func MakeTestUserId(connector gotabase.Connector) uuid.UUID {
+	id := GetRandomUuid()
+	MakeTestUser(connector, id)
+	return id
+}
+
+func MakeTestUser(connector gotabase.Connector, id uuid.UUID) {
+	query := `insert into users (id, provider_id, provider_name, email) values ($1, $2, $3, $4)`
+	_, err := connector.Exec(query, id, id.String(), id.String(), id.String())
+	PanicOnErr(err)
 }
