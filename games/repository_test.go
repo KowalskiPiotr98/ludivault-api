6309dab8313a0e128d2aef5f52eada2c2b710cf7
Piotr Kowalski <piotr.98@icloud.com>
2025-01-07 19:08:53 +0100

    Add ability to filter games by some properties

--- games/repository_test.go
+++ games/repository_test.go
@@ -2,6 +2,7 @@ package games
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/KowalskiPiotr98/gotabase/operations"
 	"github.com/KowalskiPiotr98/ludivault/internal/tests"
 	"github.com/google/uuid"
@@ -13,6 +14,14 @@ func makePlatform(userId uuid.UUID) uuid.UUID {
 	id := tests.GetRandomUuid()
 	query := `insert into platforms (id, name, short_name, user_id) values ($1, 'aa', 'aa', $2)`
 	_, err := getDatabase().Exec(query, id, userId)
+	tests.PanicOnErr(err)
+	return id
+}
+
+func makePlaythrough(gameId uuid.UUID, status int) uuid.UUID {
+	id := tests.GetRandomUuid()
+	query := `insert into playthroughs (game_id, status, start_date) values ($1, $2, now())`
+	_, err := getDatabase().Exec(query, gameId, status)
 	tests.PanicOnErr(err)
 	return id
 }
@@ -56,40 +65,42 @@ func TestCreateGame(t *testing.T) {
 }
 
 func TestGetGames(t *testing.T) {
-	t.Run("Get all games", func(t *testing.T) {
-		tests.GetDatabaseWithCleanup(t)
-		userId := tests.MakeTestUserId(getDatabase())
-		platformId := makePlatform(userId)
-		games := []Game{
-			{
-				PlatformId:  platformId,
-				Title:       "test game 1",
-				Owned:       true,
-				ReleaseDate: sql.NullTime{Valid: true, Time: tests.GetRandomTestTime()},
-				Released:    true,
-			},
-			{
-				PlatformId:  platformId,
-				Title:       "test game 2",
-				Owned:       true,
-				ReleaseDate: sql.NullTime{Valid: true, Time: tests.GetRandomTestTime()},
-				Released:    true,
-			},
-			{
-				PlatformId:  platformId,
-				Title:       "test game 3",
-				Owned:       true,
-				ReleaseDate: sql.NullTime{Valid: true, Time: tests.GetRandomTestTime()},
-				Released:    true,
-			},
-		}
-		for i := range games {
-			tests.PanicOnErr(CreateGame(&games[i], userId))
-		}
-		clone := games[0]
-		tests.PanicOnErr(CreateGame(&clone, tests.MakeTestUserId(getDatabase())))
+	tests.GetDatabaseWithCleanup(t)
+	userId := tests.MakeTestUserId(getDatabase())
+	platformId := makePlatform(userId)
+	games := []Game{
+		{
+			PlatformId:  platformId,
+			Title:       "test game 1",
+			Owned:       false,
+			ReleaseDate: sql.NullTime{Valid: true, Time: tests.GetRandomTestTime()},
+			Released:    true,
+		},
+		{
+			PlatformId:  platformId,
+			Title:       "test game 2",
+			Owned:       true,
+			ReleaseDate: sql.NullTime{Valid: true, Time: tests.GetRandomTestTime()},
+			Released:    false,
+		},
+		{
+			PlatformId:  platformId,
+			Title:       "test game 3",
+			Owned:       true,
+			ReleaseDate: sql.NullTime{Valid: true, Time: tests.GetRandomTestTime()},
+			Released:    true,
+		},
+	}
+	for i := range games {
+		tests.PanicOnErr(CreateGame(&games[i], userId))
+	}
+	makePlaythrough(games[0].Id, 0)
+	makePlaythrough(games[1].Id, 1)
+	clone := games[0]
+	tests.PanicOnErr(CreateGame(&clone, tests.MakeTestUserId(getDatabase())))
 
-		list, err := GetGames(0, 100, userId)
+	t.Run("Get all games", func(t *testing.T) {
+		list, err := GetGames(0, 100, userId, "game", nil, nil, nil)
 
 		assert.NoError(t, err)
 		assert.Len(t, list, len(games))
@@ -97,6 +108,62 @@ func TestGetGames(t *testing.T) {
 			game.ReleaseDate.Time = game.ReleaseDate.Time.UTC()
 			assert.Contains(t, games, *game)
 		}
+	})
+
+	bools := []bool{true, false}
+
+	t.Run("Get only owned", func(t *testing.T) {
+		for _, arg := range bools {
+			t.Run(fmt.Sprint(arg), func(t *testing.T) {
+				list, err := GetGames(0, 100, userId, "", tests.GetPointerFromValue(arg), nil, nil)
+
+				assert.NoError(t, err)
+				assert.NotEmpty(t, list)
+				for _, game := range list {
+					assert.Equal(t, arg, game.Owned)
+				}
+			})
+		}
+	})
+
+	t.Run("Get only released", func(t *testing.T) {
+		for _, arg := range bools {
+			t.Run(fmt.Sprint(arg), func(t *testing.T) {
+				list, err := GetGames(0, 100, userId, "", nil, tests.GetPointerFromValue(arg), nil)
+
+				assert.NoError(t, err)
+				assert.NotEmpty(t, list)
+				for _, game := range list {
+					assert.Equal(t, arg, game.Released)
+				}
+			})
+		}
+	})
+
+	t.Run("Only in progress", func(t *testing.T) {
+		list, err := GetGames(0, 100, userId, "", nil, nil, tests.GetPointerFromValue(true))
+
+		assert.NoError(t, err)
+		assert.Len(t, list, 1)
+		assert.Equal(t, games[0].Id, list[0].Id)
+	})
+
+	t.Run("Only not in progress", func(t *testing.T) {
+		list, err := GetGames(0, 100, userId, "", nil, nil, tests.GetPointerFromValue(false))
+
+		assert.NoError(t, err)
+		assert.Len(t, list, 2)
+		for _, game := range list {
+			assert.NotEqual(t, games[0].Id, game.Id)
+		}
+	})
+
+	t.Run("Filter by title", func(t *testing.T) {
+		list, err := GetGames(0, 100, userId, "1", nil, nil, nil)
+
+		assert.NoError(t, err)
+		assert.Len(t, list, 1)
+		assert.Equal(t, games[0].Id, list[0].Id)
 	})
 }
 
